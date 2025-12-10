7dbbaaf6cae326922b4357df5a90bb24e01a1151
amanqs <116822442+amanqs@users.noreply.github.com>
2025-12-10 09:20:32 +0700

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,11 +1,11 @@
 # config.py
 
-API_ID = 123456   # ganti dengan API ID my.telegram.org
-API_HASH = "your_api_hash_here"  # ganti
-BOT_TOKEN = "123456789:your_bot_token_here"  # ganti
+API_ID = 9128356   # ganti dengan API ID my.telegram.org
+API_HASH = "cf219228092173e15047d3fb5a023cfc"  # ganti
+BOT_TOKEN = "8547139242:AAHSyKp8iSRkswAl37TymgP4crlihTsqlZY"  # ganti
 
 # lokasi file SQLite
 DB_PATH = "sessions.db"
 
 # admin yang boleh pakai /admin_users (optional)
-ADMINS = [123456789]  # isi dengan user_id kamu, bisa lebih dari satu
+ADMINS = [1839010591]  # isi dengan user_id kamu, bisa lebih dari satu
