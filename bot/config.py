3336b750dc2b47299d155a419419781cab0a429e
amanqs <116822442+amanqs@users.noreply.github.com>
2025-12-10 09:55:21 +0700

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,7 +5,10 @@ API_HASH = "cf219228092173e15047d3fb5a023cfc"  # ganti
 BOT_TOKEN = "8547139242:AAHSyKp8iSRkswAl37TymgP4crlihTsqlZY"  # ganti
 
 # lokasi file SQLite
-DB_PATH = "sessions.db"
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "sessions.db")
+
 
 # admin yang boleh pakai /admin_users (optional)
 ADMINS = [1839010591]  # isi dengan user_id kamu, bisa lebih dari satu
